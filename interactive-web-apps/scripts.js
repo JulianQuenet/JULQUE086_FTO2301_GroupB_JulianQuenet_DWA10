38dfea823dd483a6404c3ba6381639933091aa6d
JulianQuenet <julianquenet8@gmail.com>
2023-03-27 15:58:20 +0200

    Added everything to folder

--- interactive-web-apps/scripts.js
+++ interactive-web-apps/scripts.js
@@ -2,12 +2,12 @@ const MAX_NUMBER= 15
 const MIN_NUMBER= - 5
 const STEP_AMOUNT= 5
 
-const number = document.querySelector([data-key="number"])
-const subtract = document.querySelector([data-key="subtract"]) 
-const add = document.querySelector([data-key="add"]) 
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]') 
+const add = document.querySelector('[data-key="add"]') 
 
 const subtractHandler = () => {
-  const new Value = parseInt(number.value) - STEP_AMOUNT
+  const newValue = parseInt(number.value) - STEP_AMOUNT
   number.value = newValue
  
   if(add.disabled === true) {
@@ -21,7 +21,7 @@ const subtractHandler = () => {
 
 
 const addHandler = () => {
-  const new Value = parseInt(number.value) + STEP_AMOUNT
+  const newValue = parseInt(number.value) + STEP_AMOUNT
   number.value = newValue
  
   if(subtract.disabled === true) {
@@ -35,4 +35,4 @@ const addHandler = () => {
 
 
 subtract.addEventListener('click', subtractHandler)
-add.addEventListner('click', addHandler) 
+add.addEventListener('click', addHandler) 
