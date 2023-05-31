f96908bc8ccef4125ef2bee14b471057945ca7b7
JulianQuenet <julianquenet8@gmail.com>
2023-05-31 16:59:17 +0200

    used shoelace components and did some styling

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
-const MAX_NUMBER= 15
-const MIN_NUMBER= - 5
-const STEP_AMOUNT= 5
+const MAX_NUMBER= Infinity
+const MIN_NUMBER= -Infinity
+const STEP_AMOUNT= 1
 
-const number = document.querySelector('[data-key="number"]')
-const subtract = document.querySelector('[data-key="subtract"]') 
-const add = document.querySelector('[data-key="add"]') 
+const number = document.querySelector('[data-number]')
+const subtract = document.querySelector('[data-subtract]') 
+const add = document.querySelector('[data-add]') 
+const reset = document.querySelector('[data-reset]')
+const message = document.querySelector('[data-message]')
+const overlay = document.querySelector('[data-overlay]')
 
 const subtractHandler = () => {
   const newValue = parseInt(number.value) - STEP_AMOUNT
@@ -13,12 +16,7 @@ const subtractHandler = () => {
   if(add.disabled === true) {
   add.disabled = false
   }
-
-  if(newValue <= MIN_NUMBER) {
-  subtract.disabled = true
-  }
 } 
-
 
 const addHandler = () => {
   const newValue = parseInt(number.value) + STEP_AMOUNT
@@ -28,11 +26,20 @@ const addHandler = () => {
   subtract.disabled = false
   }
 
-  if(newValue >= MAX_NUMBER) {
-  add.disabled = true
-  } 
 } 
+
+  const resetHandler = () => {
+    number.value = 0;
+    overlay.show();
+    setTimeout(() => {
+      overlay.close();
+    }, 1500);
+  };
+  
+
+
 
 
 subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler) 
+reset.addEventListener('click', resetHandler)
